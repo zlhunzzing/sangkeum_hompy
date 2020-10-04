3cae0363838fdf0457f7207821c64abdc32fb55e
zlhunzzing <zlhunzzing@gmail.com>
2020-10-04 12:54:10 +0900

    전체화면 가능

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,26 @@ import Guestbook from './page/Guestbook';
 /* pages */
 import Home from './page/Home';
 
+// function toggleFullScreen() {
+//   if (!document.fullscreenElement) {
+//     document.documentElement.requestFullscreen();
+//   } else {
+//     if (document.exitFullscreen) {
+//       document.exitFullscreen();
+//     }
+//   }
+// }
+
+// document.addEventListener(
+//   'keypress',
+//   function (e) {
+//     if (e.keyCode === 13) {
+//       toggleFullScreen();
+//     }
+//   },
+//   false,
+// );
+
 function App() {
   return (
     <div className="App">
